6b7028a2c8735b057586b7aedebf4645b6a92c32
omarkhaled11 <omar.mohamed@gls-studio.com>
2025-07-28 18:02:21 +0200

    remove ga4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  },
-  integrations: [
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    })
-  ]
+  }
 });
